0ba1e2808fa38ae06267a18acabcc604b7f333b3
Kevin Xu <khxu@berkeley.edu>
2017-08-24 14:09:18 -0700

    fix guidelines copy

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
+import { Image } from 'react-bootstrap'
 
 export default class Home extends Component {
   render () {
     return (
       <div>
         <h2>Welcome to the New Business Practicum's Office Hours Scheduler!</h2>
-        <h3>Signing Up</h3>
+        <h3>Signing Up and Troubleshooting</h3>
         <p>If you're looking to help out at office hours, please feel free to create an account by clicking "Register" on the navigation bar at the top of this screen. After logging in, click "Scheduler" to sign up for open timeslots, or cancel a session you signed up for previously.</p>
+        <p>Please contact Kevin Xu (khxu@law.berkeley.edu) if you need any help registering an account or using the Scheduler.</p>
         <h3>Guidelines</h3>
-        <p>First, THANK YOU for taking the time to help provide this important service to East Bay and Central Valley entrepreneurs of limited means. These office hours play a vital role within the framework of services we offer at the Practicum by assisting clients with short service tasks, such as delivering template documents and providing initial legal assessments. Please read on to learn more about client questions that will likely come up, how office hours are structured, the dress code, and expectations for both students and the supervising attorney.</p>
+        <p>First, THANK YOU for taking the time to help provide this important service to East Bay and Central Valley entrepreneurs of limited means. These office hours play a vital role within the framework of services we offer at the Practicum by assisting clients with limited service tasks, such as delivering template documents and providing initial answers to legal questions. Our goal is to give you opportunities to take the lead in both diagnosing and responding to entrepreneur legal needs, always with solid backup from a supervising attorney. Please read on to learn more about client questions that will likely come up, how office hours are structured, the dress code, and expectations for students.</p>
         <h4>Clients’ Needs</h4>
         <p>While clients are not pre-screened, clients in previous sessions frequently ask about the following topics:
           <ul>
@@ -20,21 +22,19 @@ export default class Home extends Component {
           </ul>
         </p>
         <h4>Office Hours Structure</h4>
-        <p>Office hours are held from 1:00 - 5:00 PM, with client sessions starting at the beginning of each hour.</p>
+        <p>Office hours are currently held from 1:00 - 5:00 PM M & W, with client sessions starting at the beginning of each hour.</p>
         <p>Client sessions have the following structure (taking the 1:00 - 2:00 PM timeslot as an example):
           <ul>
-            <li><b>Phase 1. (15 minutes)</b> 1:00 - 1:15 PM. Initial client intake. Law students and supervising attorney ask fact-gathering questions. No legal advice/answers are provided to the client during this phase.</li>
-            <li><b>Phase 2. (10 minutes)</b> 1:15 - 1:25 PM. Team discussion. The client is asked to step outside while the students and supervising attorney discuss issues spotted during Phase 1. Students divvy up the issues and prepare to explain their advice and reasoning to the client.</li>
-            <li><b>Phase 3. (15 minutes)</b> 1:25 - 1:40 PM. Legal advice. Law students provide legal advice to the client, while the supervising attorney chimes in as-needed. Limited service follow-up tasks can be offered to the client, such as sending template documents and educational slide decks.</li>
-            <li><b>Phase 4. (20 minutes)</b> 1:40 - 2:00 PM. Reflection and feedback. Law students ask the supervising attorney clarifying questions with respect to unfamiliar legal issues that arose during the client session. Law students and the supervising attorney provide feedback to one another regarding the client session. Law students write up and send a list of follow-up tasks to Kevin Xu (khxu@law.berkeley.edu), who will primarily be responsible for following up (although volunteers for follow-up tasks are always welcome).</li>
+            <li><b>Diagnostic Interview. (15 minutes)</b> 1:00 - 1:15 PM. Law students take the lead to ask fact-gathering questions based on the BECOME diagnostic. (No legal advice/answers are provided yet.)</li>
+            <li><b>Student-Supervisor Private Consult. (10 minutes)</b> 1:15 - 1:25 PM. The client is asked to step outside while the students and Supervisor (currently Kevin Xu) discuss issues spotted during the Diagnostic Interview. Students divvy up the issues and prepare to provide their advice and reasoning to the client with guidance from the Supervisor.</li>
+            <li><b>Legal Advice. (15 minutes)</b> 1:25 - 1:40 PM. Law students provide legal advice to the client, while the Supervisor chimes in as-needed. Limited service follow-up tasks can be offered to the client, such as sending template documents and educational slide decks.</li>
+            <li><b>Feedback. (20 minutes)</b> 1:40 - 2:00 PM. Law students ask the Supervisor clarifying questions with respect to unfamiliar legal issues that arose during the client session. Law students and the Supervisor provide feedback to one another regarding the client session. Law students write up and send a list of follow-up tasks to Kevin Xu (khxu@law.berkeley.edu), who will primarily be responsible for following up (although volunteers for follow-up tasks are always welcome).</li>
           </ul>
         </p>
         <h4>Dress Code</h4>
         <p>Please come to the client session dressed in business casual attire. This is important for maintaining a professional law office atmosphere. If you forget to bring a set of business casual clothes, and can’t borrow clothing from friends, please let Kevin Xu (khxu@law.berkeley.edu) know ASAP so that you can reschedule for another date.</p>
         <h4>Student Expectations</h4>
-        <p>Our goal with these legal office hours is to provide experiential learning opportunities beyond what students currently find through the New Business Practicum’s full service representation services. As such, students are expected to take the lead on managing each client session, asking exploratory questions and providing legal advice (with the oversight of the supervising attorney). Students will ensure that follow-up tasks are sent to Kevin Xu (khxu@law.berkeley.edu) within 24 hours after the client session.</p>
-        <h4>Supervising Attorney Expectations</h4>
-        <p>The supervising attorney will oversee all legal advice provided during the session and give meaningful feedback to students.</p>
+        <p>Our goal with these legal office hours is to provide additional experiential learning opportunities to help you develop more comfort and experience practicing law (under supervision). As such, students are expected to take the lead on managing each client session, asking exploratory questions and providing legal advice (with the oversight of the Supervisor). Students will ensure that follow-up tasks are sent to Kevin Xu (khxu@law.berkeley.edu) within 24 hours after the client session.</p>
       </div>
     )
   }
